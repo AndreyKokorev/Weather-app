0ffa8883880bf2484e81755d84286b2baf503ded
andreykokorev <andrey.kokorev.w.dev@gmail.com>
2020-05-04 17:37:31 +0700

    fix: searching according to selected meas. units

--- assets/js/locationSearch.js
+++ assets/js/locationSearch.js
@@ -23,7 +23,7 @@ function locationSearch() {
     if (e.target.closest('LI')) {
       const interval = setInterval(() => {
         if (data.geocoder.mapMarker) {
-          if (data.geocoder.mapMarker._lngLat){
+          if (data.geocoder.mapMarker._lngLat) {
             find();
             clearInterval(interval)
           }
@@ -33,33 +33,21 @@ function locationSearch() {
   });
 
   function find() {
-    // const lang = {
-    //   en: 'Check the input is correct',
-    //   ru: 'Проверьте правильность ввода',
-    //   be: 'Праверце правільнасць ўводу'
-    // }
     search();
-    
+
     async function search() {
-      //try {
-        console.log(data)
-        data.coords.latitude = data.geocoder.mapMarker._lngLat.lat;
-        data.coords.longitude = data.geocoder.mapMarker._lngLat.lng;
-        await getWeatherData();
-        await getSearchData();
-        getDate();
-        dataRender.locationRender();
-        dataRender.timeRender()
-        dataRender.daysRender();
-        dataRender.weatherRender();
-        dataRender.coordsRender();
-        console.log(data)
-        if (data.unitsOfMeas === 'F') unitsOfMeas.toFahrenheit();
-      // } catch (e) {
-      //   throw new Error(e);
-      // } finally {
-      //   //alert(`${lang[data.lang]}`)
-      // }
+      data.coords.latitude = data.geocoder.mapMarker._lngLat.lat;
+      data.coords.longitude = data.geocoder.mapMarker._lngLat.lng;
+      await getWeatherData();
+      await getSearchData();
+      getDate();
+      dataRender.locationRender();
+      dataRender.timeRender()
+      dataRender.daysRender();
+      dataRender.weatherRender();
+      dataRender.coordsRender();
+
+      if (data.unitOfMeas === 'F') unitsOfMeas.toFahrenheit();
     }
   }
 }
